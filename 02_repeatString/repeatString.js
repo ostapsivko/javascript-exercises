5e0b21a189dfc325c63d5513bed692fd8e544d3f
ostapsivko <o-stap@hotmail.com>
2024-09-01 22:31:34 +0300

    Add solutions for the repeatString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(value, count) {
+    if(count < 0) {
+        return 'ERROR';
+    }
+    
+    return value.repeat(count);
 };
 
 // Do not edit below this line
