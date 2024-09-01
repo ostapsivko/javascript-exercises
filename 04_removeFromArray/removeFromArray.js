a774e51c782f5ca0491933e2103f3e8a315c4638
ostapsivko <o-stap@hotmail.com>
2024-09-01 23:09:52 +0300

    Add solution for remoteFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...toRemove) {
+    let result = [];
+    let isEqual = false;
+    input.forEach(element => {
+        toRemove.forEach(item => {
+            if(element === item) {
+                isEqual = true;
+            }
+        });
+
+        if(!isEqual) {
+            result.push(element);
+        }
+
+        isEqual = false;
+    });
+
+    return result;
 };
 
 // Do not edit below this line
