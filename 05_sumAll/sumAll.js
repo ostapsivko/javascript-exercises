9098dceca46cac45efd41989effd67c3f9db8be8
ostapsivko <o-stap@hotmail.com>
2024-09-01 23:26:10 +0300

    Add solution for sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    if(typeof from === "Number" || typeof to === "Number") {
+        return 'ERROR';
+    }
+    
+    if(!Number.isInteger(from) || !Number.isInteger(to)) {
+        return 'ERROR';
+    }
 
+    if(from < 0 || to < 0) {
+        return 'ERROR';
+    }
+    
+    let sum = 0;
+
+    if(to < from) {
+        let tmp = to;
+        to = from;
+        from = tmp;
+    }
+
+    for(i = from; i <= to; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
