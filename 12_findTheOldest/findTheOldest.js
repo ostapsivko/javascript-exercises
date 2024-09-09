e728aa16ca2d71fc04d18d5d8d0af2e5c999d6bd
ostapsivko <o-stap@hotmail.com>
2024-09-10 01:28:09 +0300

    Found the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    if(!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
 
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
+const findTheOldest = function(people) {
+    return people.sort((a, b) => getAge(b) - getAge(a))[0];
 };
 
 // Do not edit below this line
