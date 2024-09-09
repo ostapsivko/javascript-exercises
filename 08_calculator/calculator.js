b8facc5a24f26567b382d47a65d3033fb783e589
ostapsivko <o-stap@hotmail.com>
2024-09-10 00:50:20 +0300

    Add solutions for calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+	return values.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+  return values.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  let result = 1;
+
+  for(let i = 1; i <= value; i++) {
+    result = i * result;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
