6ea2fc881a48b26ce27dff9822cdaa2517a9e2ba
ostapsivko <o-stap@hotmail.com>
2024-09-10 01:12:29 +0300

    Add solutions for palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let allowed = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let chars = input.toLowerCase().split('').filter(c => allowed.includes(c));
+    let isPalindrome = true;
 
+    for(let i = 0; i < chars.length / 2; i++) {
+        if(chars[i] !== chars[chars.length - i - 1]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
