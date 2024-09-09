1c795edd996f7d36605e1922febd5b845b21a0f8
ostapsivko <o-stap@hotmail.com>
2024-09-10 01:18:21 +0300

    Add fibonacci seq calculation

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let base0 = 0; let base1 = 1;
+    let end = Number(number);
 
+    if(end < 0) {
+        return 'OOPS';
+    }else if(end === 0) {
+        return base0;
+    } else if(end === base1) {
+        return base1;
+    }
+
+    return fibonacci(end - 1) + fibonacci(end - 2);
 };
 
 // Do not edit below this line
